de47a5e858439235cad2e21c08ef211842d7912f
muhammadfajri-tif <149763217+muhammadfajri-tif@users.noreply.github.com>
2024-05-18 21:47:20 +0700

    style: remove semicolon and use double quotes

--- lib/api-service.handler.ts
+++ lib/api-service.handler.ts
@@ -1,30 +1,31 @@
-import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
-import { APIGatewayProxyEventV2WithRequestContext, Context } from 'aws-lambda'
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3"
+import { APIGatewayProxyEventV2WithRequestContext, Context } from "aws-lambda"
 
 interface Rate {
-  buy?: number;
-  sell?: number;
+  buy?: number
+  sell?: number
 }
 
 interface ExchangeRate {
-  type: "bank notes" | "dd/tt" | "special rates";
-  bank: string;
-  date: number;
+  type: "bank notes" | "dd/tt" | "special rates"
+  bank: string
+  date: number
   IDRExchangeRate: {
-    USD: Rate;
-    SGD: Rate;
-    EUR: Rate;
-    CNY: Rate;
-    GBP: Rate;
-    JPY: Rate;
-    SAR: Rate;
+    USD: Rate
+    SGD: Rate
+    EUR: Rate
+    CNY: Rate
+    GBP: Rate
+    JPY: Rate
+    SAR: Rate
   }
 }
 
 const client = new S3Client({ region: String(process.env.REGION) })
 
 const getDataFromS3 = async (): Promise<ExchangeRate[]> => {
-  const appEnv = String(process.env.APP_ENV) === 'prod' || 'production' ? 'prod' : 'dev'
+  const appEnv =
+    String(process.env.APP_ENV) === "prod" || "production" ? "prod" : "dev"
 
   const command = new GetObjectCommand({
     Bucket: process.env.BUCKET_NAME,
@@ -35,19 +36,22 @@ const getDataFromS3 = async (): Promise<ExchangeRate[]> => {
     const response = await client.send(command)
     const payload = await response.Body?.transformToString()
 
-    return payload ? JSON.parse(payload) : ''
+    return payload ? JSON.parse(payload) : ""
   } catch (err) {
-    console.error('Error when retrieve data from S3.', err)
+    console.error("Error when retrieve data from S3.", err)
 
     throw new Error(`Cannot Get Data from S3. Error: ${err}`)
   }
 }
 
-export const handler = async (event: APIGatewayProxyEventV2WithRequestContext<any>, context: Context) => {
+export const handler = async (
+  event: APIGatewayProxyEventV2WithRequestContext<any>,
+  context: Context
+) => {
   let statusCode
   let body
   const headers = {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   }
 
   try {
@@ -56,12 +60,12 @@ export const handler = async (event: APIGatewayProxyEventV2WithRequestContext<an
     if (data.length !== 0 || data) {
       statusCode = 200
       body = {
-        payload: data
+        payload: data,
       }
     } else {
       statusCode = 404
       body = {
-        message: "Data from S3 not found."
+        message: "Data from S3 not found.",
       }
     }
 
@@ -70,9 +74,15 @@ export const handler = async (event: APIGatewayProxyEventV2WithRequestContext<an
       multiValueHeaders: null,
       statusCode,
       headers,
-      body: JSON.stringify(body)
+      body: JSON.stringify(body),
     }
-    console.log(`[HANDLER] ${statusCode === 200 ? 'Success Retrieve Data from S3 and Invoke Function.' : 'Failed.'}`)
+    console.log(
+      `[HANDLER] ${
+        statusCode === 200
+          ? "Success Retrieve Data from S3 and Invoke Function."
+          : "Failed."
+      }`
+    )
     return response
   } catch (err) {
     console.error("[API Service]", err)
@@ -82,8 +92,8 @@ export const handler = async (event: APIGatewayProxyEventV2WithRequestContext<an
       multiValueHeaders: null,
       headers,
       body: JSON.stringify({
-        message: "Something went wrong."
-      })
+        message: "Something went wrong.",
+      }),
     }
     return response
   }
